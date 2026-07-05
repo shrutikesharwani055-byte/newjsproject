f876f5be835d37927e4278b50ab58a005bf29522
shrutikesharwani055-byte <shrutikesharwani055@gmail.com>
2026-07-05 17:47:08 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,12 @@
+"use strict" // treat all js code as a newer version
+
+// // alert("hello") // we use nodejs not browser
+// string
+//int
+//bigint
+//boolean
+//null
+//undefined
+//symbol =>unique
+console.log(typeof undefined) // undefined     (when variable declar but not assigned)
+console.log(typeof null) //object
---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountNumber=23456
+let accountEmail="shruti@123"
+var accountPassword="123456"
+accountCity = "jaipur"
+let accountnum
+// accountNumber=22 
+accountEmail="shr"
+accountPassword="ajgdwu"
+accountCity="jsdjak"
+// console.log(accountEmail)
+
+
+/*
+prefer not to use var 
+because of block scope and functional scope
+*/
+
+console.table([accountNumber,accountEmail,accountPassword,accountCity,accountnum])
